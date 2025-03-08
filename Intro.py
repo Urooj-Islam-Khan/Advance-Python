e9c67161013cee9d421d6113dc33601930bcfe40
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2025-03-08 22:35:55 +0500

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,35 @@
+# What is Programming?
+#     Programming is a way to instruct the computer to perform various tasks.
+
+# What is Python?
+#   Python is a simple and easy to understand language which feels like reading simple english.
+
+#  Features of Python 
+# Easy to understand = less determined Time
+# Open Source
+# High Level Language
+# Portable -> works on Linux/Window/Mac
+
+import os   #this is built in module
+
+print("Hello World")  # print is function used to print a statement
+
+# Modules:
+#     A module is a file containing code written by somebody else(usually) which can be imported and used in our programs
+
+# Pop:
+# pip is the package manager for python you can use pip to install a module on your system
+
+# Types of modules
+# there are two types of modules in Python
+# 1> Built in Modules (Pre installed in python)
+# 1> External Modules (Need to install using pip)
+
+
+# Comments:
+#  Comments are used to write something which the programmer does not want to execute.
+
+# Types of Comments:
+# 1: Single Line Comments  -> written using # comment 
+# 2: Multi Line Comments -> written using '''comment'''  
+
